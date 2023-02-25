3b23f60f6be60fa2697d8d037c49814e25fd2d33
baronin <baronin.yaroslav@gmail.com>
2023-02-25 23:09:26 +0200

    chore: add locations api

--- src/api/index.js
+++ src/api/index.js
@@ -1,9 +1,11 @@
 import { flightOffers } from '@/api/flight-offers'
 import { posts } from '@/api/posts'
 import { auth } from '@/api/auth'
+import { locations } from '@/api/locations'
 
 export const api = {
   flightOffers,
   posts,
   auth,
+  locations,
 }
